ab2c4ead0c258da75cc080f497beecdffbd2ff8e
Santiago Rodriguez Angel <127535936+SantiagoRodriguezAngel@users.noreply.github.com>
2025-06-03 15:26:13 -0500

    mejora

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -19,6 +19,7 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     sourcemap: true,
+    assetsInclude: ['**/*.svg'],
     rollupOptions: {
       output: {
         manualChunks: {
